d929d50b0ca2a175eec564f9e06b86eb7cbd94bf
Halina Tulchii <halendeihalyna@gmail.com>
2025-04-02 18:43:23 +0300

    added arrays tasks

--- js/array.js
+++ js/array.js
@@ -1,3 +1,67 @@
 function array(){
-    
+//     let fruits = ["Apples", "Pear", "Orange"];
+
+// let shoppingCart = fruits;
+// shoppingCart.push("Banana");
+
+// alert( fruits.length );
+
+
+// let styles = ["Jazz", "Blues"]
+// console.log(styles)
+
+// styles.push("Rock-n-Roll")
+// console.log(styles)
+
+// const arrayLength = styles.length
+// const middleIndex = Math.floor(arrayLength/2)
+// styles[middleIndex] = "Classics"
+// console.log(styles)
+
+// const firstEl = styles.shift()
+// console.log(firstEl); 
+// console.log(styles); 
+
+
+// styles.unshift("Rap", "Reggae")
+// console.log(styles); 
+
+
+// let arr = ["a", "b"];
+
+// arr.push(function() {
+//   alert( this );
+// });
+
+// arr[2]();
+
+
+// function sumInput(){
+//     let message = prompt('please enter some number', '')
+//     let array = []
+//     if (message === '' || message === null || isNaN(+message)) {
+//         return null;
+//     }else{
+        
+//         array.push(+message)
+//     }
+
+//     let sum = 0
+//     for(let i = 0; i < array.length; i++){
+//         sum += Number(array[i])
+//     }
+
+//     return alert(sum)
+// }
+// sumInput([1, 2, 3, 4, 5])
+
+
+
+function getMaxSubSum(arr){
+
 }
+}
+
+array()
+
+export default array
